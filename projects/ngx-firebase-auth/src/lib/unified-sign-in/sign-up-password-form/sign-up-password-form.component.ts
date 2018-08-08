fdb156ddd95669e5b3eaf990c43c463b1b32057f
Christopher Carson <chris@nowzoo.com>
2018-08-08 09:49:45 -0500

    simplifying

--- projects/ngx-firebase-auth/src/lib/unified-sign-in/sign-up-password-form/sign-up-password-form.component.ts
+++ projects/ngx-firebase-auth/src/lib/unified-sign-in/sign-up-password-form/sign-up-password-form.component.ts
@@ -1,11 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone, Inject, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormControl, Validators, ValidationErrors } from '@angular/forms';
+import { auth } from 'firebase/app';
+import { take } from 'rxjs/operators';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NgxFirebaseAuthService } from '../../ngx-firebase-auth.service';
+import { NgxFormUtils, NgxFormValidators } from '@nowzoo/ngx-form';
 
 @Component({
-  selector: 'lib-sign-up-password-form',
+  selector: 'ngx-firebase-auth-sign-up-password-form',
   templateUrl: './sign-up-password-form.component.html',
   styleUrls: ['./sign-up-password-form.component.css']
 })
 export class SignUpPasswordFormComponent implements OnInit {
+
+  formId = 'ngx-firebase-auth-sign-up-password-form-';
+  submitting = false;
+  nameFc: FormControl;
+  passwordFc: FormControl;
+  fg: FormGroup;
+  showInvalid = NgxFormUtils.showInvalid;
+
+  @Input() email: string;
+  @Output() success: EventEmitter<auth.UserCredential> = new EventEmitter();
+  @Output() error: EventEmitter<auth.Error> = new EventEmitter();
 
   constructor() { }
 
